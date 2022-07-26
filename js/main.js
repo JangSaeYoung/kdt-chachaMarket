78a139c2892901671a6ab59c67da18a2fa4225e0
saeyoung <syoungbb25@gmail.com>
2022-07-27 08:46:11 +0900

    contents 추가, img 추가, js 스와이퍼 추가

--- js/main.js
+++ js/main.js
@@ -1,3 +1,7 @@
+window.onload = () => {
+  alert('맥북프로13 과 아이폰12pro 기준으로 봐주세요!!!');
+};
+
 const subMenu = document.querySelector('#snb .sub_menu');
 const snbBtn = document.querySelector('#snb .btn_menu');
 const modal = document.querySelector('.modal_bg');
@@ -10,4 +14,24 @@ snbBtn.addEventListener('click', function () {
     subMenu.classList.add('hide');
     modal.classList.remove('hide');
   }
+});
+
+const swiperPapper = new Swiper('.papper .swiper', {
+  direction: 'horizontal',
+  slidesPerView: 4,
+  spaceBetween: 10,
+  centeredSlides: true,
+  loop: true,
+  autoplay: {
+    delay: 3000,
+    disableOnInteraction: false,
+  },
+  pagination: {
+    el: '.papper .swiper-pagination',
+    clickable: true,
+  },
+  navigation: {
+    prevEl: '.papper .swiper-button-prev',
+    nextEl: '.papper .swiper-button-next',
+  },
 });
